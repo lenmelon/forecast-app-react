2fd9dcb3eed5b51f75cb620de00f22dcf358e768
lenmelon <lenmelon@gmail.com>
2024-12-07 12:39:48 +0000

    New repository created to fix errors and for cleanup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Lisbon" />
+        <footer>
+          This project was created by Elena F. an is open-sourced on{" "}
+          <a
+            href="https://github.com/lenmelon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
