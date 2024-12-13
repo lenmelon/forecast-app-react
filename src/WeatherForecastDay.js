e825a30d4e54d6d5f4ebc141efdf65e7e2bf9dd7
lenmelon <lenmelon@gmail.com>
2024-12-13 10:25:51 +0000

    API error fixed

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -24,7 +24,7 @@ export default function WeatherForecastDay(props) {
   return (
     <div>
       <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIcon code={props.data.condition.icon} size={36} />
+      <WeatherIcon code={props.data.condition.icon} size={40} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperature-max">
           {maxTemperature()}
